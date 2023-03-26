89f5c3da0392775295709a4aeae7ed0d2b713d43
Rajasekhar_Mutukuri <rajasekhar_muthukuri@capturisk.com>
2023-03-25 23:32:00 -0400

    updated

--- UserAccountManagement/main.go
+++ UserAccountManagement/main.go
@@ -1,24 +1,15 @@
 package main
 
 import (
-	// "database/sql"
 	"fmt"
 	"net/http"
 
-	// _ "github.com/go-sql-driver/mysql"
 	Dbconfig "github.com/sravan2509/Customer/tree/main/UserAccountManagement/Dbconfig"
 	// Handler "github.com/sravan2509/Customer/tree/main/UserAccountManagement/Endpoints"
 )
 
 func main() {
 	Dbconfig.DBConnection
-
-	// db, err := sql.Open("mysql", "root:Sravan@2509@tcp(localhost:3306)/golang")
-	// if err != nil {
-	// 	fmt.Println("Error in connecting the DB", err)
-	// }
-	// fmt.Println("Database is connected successfully")
-	// defer db.Close()
 
 	// //handling routes
 	// http.HandleFunc("/Signup", Handler.signupHandler)
