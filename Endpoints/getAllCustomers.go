5535cf26f6be2520bd64d63f783289640401d099
Sravan <sravansatish2509@gmail.com>
2023-03-26 23:36:11 -0400

    complete updated  code without Authorization

--- Endpoints/getAllCustomers.go
+++ Endpoints/getAllCustomers.go
@@ -9,6 +9,13 @@ import (
 
 func GetAllCustomersHandler(w http.ResponseWriter, r *http.Request) {
 
+	//valiadte method
+	if r.Method != "GET" {
+		ResponseFormat(w, "Method not allowed", http.StatusMethodNotAllowed, nil)
+		return
+	}
+
+	//DB connection
 	db, err := Dbconfig.DBConnection()
 	if err != nil {
 		ResponseFormat(w, "DB Connection Failed", http.StatusInternalServerError, nil)
@@ -16,10 +23,13 @@ func GetAllCustomersHandler(w http.ResponseWriter, r *http.Request) {
 	}
 	defer db.Close()
 
+	//Get all customers
 	StatusCode, err, Result := Dbconfig.GetCustomers(db)
 	if err != nil {
 		ResponseFormat(w, err.Error(), StatusCode, nil)
 	}
+
+	//formating the result to []Schema.Customer
 	var customers []Schema.Customer
 	for Result.Next() {
 		var customer Schema.Customer
@@ -30,6 +40,7 @@ func GetAllCustomersHandler(w http.ResponseWriter, r *http.Request) {
 		}
 		customers = append(customers, customer)
 	}
+
 	ResponseFormat(w, "Customers", http.StatusOK, customers)
 
 }
